fccde1faea85d0ae760ef537df7650eb6bac8012
sanjaygaire <gairesanjay59@gmail.com>
2025-05-31 12:35:32 +0545

    initialized

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+import React from 'react'
+import Home from './componentss/Home'
+
+const App = () => {
+  return (
+    <>
+    <div>
+      <h2>
+        hi this is sanjay
+      </h2>
+    </div>
+    <Home></Home>
+    <Home></Home>
+    
+    </>
+  )
+}
+
+export default App
